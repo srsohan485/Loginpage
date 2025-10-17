c936cb8743828f058ed092e68e4ffbbe4081b0fe
srsohan485 <sayedurrahmansohan@gmail.com>
2025-10-17 21:03:58 +0600

    Simple Calculation1

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,161 @@
 import 'package:flutter/material.dart';
 
-import 'math1.dart';
-import 'math2.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
 
-
-void main() => runApp(SimpleInterestApp());
-
-class SimpleInterestApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FuelCostCalculator(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
-class SimpleInterestCalculator extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+
   @override
-  _SimpleInterestCalculatorState createState() =>
-      _SimpleInterestCalculatorState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _SimpleInterestCalculatorState extends State<SimpleInterestCalculator> {
-  final principalController = TextEditingController();
-  final rateController = TextEditingController();
-  final timeController = TextEditingController();
+class _MyHomePageState extends State<MyHomePage> {
+  final _formkey=GlobalKey<FormState>();
 
-  double totalInterest = 0;
-  double finalAmount = 0;
+  TextEditingController nameController=TextEditingController();
+  TextEditingController emailController=TextEditingController();
+  TextEditingController passwordController=TextEditingController();
 
-  void calculateInterest() {
-    double p = double.tryParse(principalController.text) ?? 0;
-    double r = double.tryParse(rateController.text) ?? 0;
-    double t = double.tryParse(timeController.text) ?? 0;
+  FocusNode nameFocusNode=FocusNode();
+  FocusNode emailFocusNode=FocusNode();
+  FocusNode passwordFocusNode=FocusNode();
 
-    setState(() {
-      totalInterest = (p * r * t) / 100;
-      finalAmount = p + totalInterest;
-    });
-  }
+  var result="";
+
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(title: Text("Interest Calculator")),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            SizedBox(height: 10),
-            TextFormField(
-              controller: principalController,
-              decoration: InputDecoration(
-                  labelText: "Principal",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10))
-                  )),//Darneya tk
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              controller: rateController,
-              decoration: InputDecoration(
-                labelText: "Rate (%)",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10))
-                  )
-
-              ),//Suder tk
-              keyboardType: TextInputType.number,
-
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              maxLines: 1,
-              controller: timeController,
-              decoration: InputDecoration(
-                labelText: "Time (years)",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10))
-                  )
-
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              //-----EditText Box----------
+              TextFormField(
+                controller: nameController,
+                focusNode: nameFocusNode,
+                maxLines: 1,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                    labelText: "Name",
+                    hintText: "Name",
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                    )
+                ),
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return ("Please Enter Your Name");
+                  }
+                  return null;
+                },
               ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: calculateInterest,
-              child: Text("Calculate"),
-            ),
-            SizedBox(height: 20),
-            Text("Total Interest: $totalInterest"),
-            Text("Final Amount: $finalAmount"),
-          ],
+              //------EditText Box----------------
+              SizedBox(height: 10),
+              TextFormField(
+                controller: emailController,
+                focusNode: emailFocusNode,
+                maxLines: 1,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Email",
+                    prefixIcon: Icon(Icons.email),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                    )
+                ),
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return ("Please Enter Your Email");
+                  }
+                  return null;
+                },
+              ),
+              //------EditText Box----------------
+              SizedBox(height: 10),
+              TextFormField(
+                controller: passwordController,
+                focusNode: passwordFocusNode,
+                textInputAction: TextInputAction.done,
+                maxLines: 1,
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Password",
+                    suffix: Icon(Icons.password),
+                    prefixIcon: Icon(Icons.remove_red_eye),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                    )
+                ),
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return ("Please Enter Your Password");
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                  onPressed: (){
+                    if(!_formkey.currentState!.validate()){
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Please Input All The Faild')),
+                      );
+                    }
+                    print("Name: ${nameController.text}");
+                    print("Email: ${emailController.text}");
+                    print("Password: ${passwordController.text}");
+
+                    setState(() {
+                      result="Name: ${nameController.text} Email: ${emailController.text} Password: ${passwordController.text}";
+                    });
+                  },
+                  child: Text("Submit Form")
+              ),
+              SizedBox(height: 10),
+              Text("${result}"),
+
+
+            ],
+
+
+          ),
+
         ),
       ),
+
     );
   }
 }
